df68f28047466aba8a424c5ae6f736a1da9a22f4
Matthew Meredith <merematt@udel.edu>
2023-02-14 10:31:33 -0500

    publishAll

--- src/nested.ts
+++ src/nested.ts
@@ -114,7 +114,10 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * each question is now published, regardless of its previous published status.
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    return [...questions].map((question: Question) => ({
+        ...question,
+        published: true
+    }));
 }
 
 /***
