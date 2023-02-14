ff4f92b6994e15e3d8a5dff6a06717ed4ed615be
Matthew Meredith <merematt@udel.edu>
2023-02-14 10:27:02 -0500

    sumPublishedPoints

--- src/nested.ts
+++ src/nested.ts
@@ -60,14 +60,17 @@ export function getNames(questions: Question[]): string[] {
 export function sumPoints(questions: Question[]): number {
     return [...questions]
         .map((question: Question) => question.points)
-        .reduce((previous: number, current: number) => previous + current);
+        .reduce((previous: number, current: number) => previous + current, 0);
 }
 
 /***
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    return [...questions]
+        .filter((question: Question) => question.published)
+        .map((question: Question) => question.points)
+        .reduce((previous: number, current: number) => previous + current, 0);
 }
 
 /***
