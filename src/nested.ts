5f4a87a7d1500693d9ab1727fbbb7fa60e9ce61d
Matthew Meredith <merematt@udel.edu>
2023-02-14 10:26:07 -0500

    sumPoints

--- src/nested.ts
+++ src/nested.ts
@@ -58,7 +58,9 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    return [...questions]
+        .map((question: Question) => question.points)
+        .reduce((previous: number, current: number) => previous + current);
 }
 
 /***
