6b3b6897c447b501afc8e5809bb3480e05eecd4b
Matthew Meredith <merematt@udel.edu>
2023-02-14 00:18:13 -0500

    isValid

--- src/objects.ts
+++ src/objects.ts
@@ -42,7 +42,10 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    return (
+        question.type === "short_answer_question" ||
+        question.options.includes(answer)
+    );
 }
 
 /**
